a6bb52e1e372756075ec6d0e0cf745b53f3d6946
sondley <32400754+sondley@users.noreply.github.com>
2018-11-13 21:04:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json())
 const db = require('./app/config/db.config.js');
   
 // force: true will drop the table if it already exists
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync({}).then(() => {
   console.log('Drop and Resync with { force: true }');
 });
  
