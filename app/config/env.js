af68864637a1518d56e445f4f56061aaa4063d7e
Sondley Northecide <sondley1@gmail.com>
2018-11-13 20:58:27 -0400

    first commit

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'sql10265064',
+  username: 'sql10265064',
+  password: 'czlbkA6aYF',
+  host: 'sql10.freemysqlhosting.net',
+  dialect: 'mysql',
+  pool: {
+	  max: 5,
+	  min: 0,
+	  acquire: 30000,
+	  idle: 10000
+  }
+};
+ 
+module.exports = env;
