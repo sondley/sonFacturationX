0e6255ee76e964fc96d04e02763902d99cf31287
sondley <32400754+sondley@users.noreply.github.com>
2018-11-13 21:18:16 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ require('./app/route/customer.route.js')(app);
 // Create a Server
 var server = app.listen(8081, function () {
  
-  var host = 'https://api-facturationx.heroku.com/'
+  var host = 'https://api-facturationx.heroku.com'
   var port = server.address().port
  
   console.log("App listening at http://%s:%s", host, port)
